806b209f65f67adc02e07d1637330dddffdf3790
Mylo1771 <stewartaustin1771@gmail.com>
2026-08-13 09:08:41 -0500

    Implemented code to determine random computer choice for R-P-S game
    Not much time to work on project today so this is all I've got for today

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-console.log('hello world!');
-console.log('my balls itch');
+let playerScore;
+let computerScore;
+
+let randomHand = Math.floor(Math.random() * 12);
+
+function getComputerChoice(int) {
+    if (int <= 3) {
+        console.log('Computer throws...paper!')
+    } else if (int > 3 && int <= 7) {
+        console.log('Computer throws...Rock')
+    } else if (int > 7 && int <= 11) {
+        console.log('Computer throws...Scissors')
+    } else {
+        console.log('Erm, that just happened')
+    }
+}
+
+function getPlayerChoice() {
+    let playerHand = prompt('What hand would you like to play?', "");
+}
+
+getComputerChoice(randomHand);
