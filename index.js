15ce4ae35c759e3e7388866dfe79d36fb902bc15
Mylo1771 <stewartaustin1771@gmail.com>
2026-08-14 13:48:03 -0500

    Completing the game logic for 5 rounds of RPS
    Knowledge of for loops is limited so I used a decremental count system

--- index.js
+++ index.js
@@ -1,22 +1,96 @@
-let playerScore;
-let computerScore;
+function getPlayerChoice() {
+    playerHand = prompt('What hand would you like to play?', '').toLowerCase();
+    console.log('Player chose... ' + playerHand);
+    return playerHand
+    
+}
 
-let randomHand = Math.floor(Math.random() * 12);
-
-function getComputerChoice(int) {
-    if (int <= 3) {
-        console.log('Computer throws...paper!')
-    } else if (int > 3 && int <= 7) {
-        console.log('Computer throws...Rock')
-    } else if (int > 7 && int <= 11) {
-        console.log('Computer throws...Scissors')
+function getComputerChoice() {
+    let computerHand = Math.floor(Math.random() * 12);
+    if (computerHand <= 3) {
+        alert('Computer chose...paper!')
+        console.log('Computer chose...paper!')
+        console.log("#################")
+        return 'paper';
+    } else if (computerHand > 3 && computerHand <= 7) {
+        alert('Computer chose...rock!')
+        console.log('Computer chose...rock!')
+        console.log("#################")
+        return 'rock';
+    } else if (computerHand > 7 && computerHand <= 11) {
+        alert('Computer chose...scissors!')
+        console.log('Computer chose...scissors!')
+        console.log("#################")
+        return 'scissors'
     } else {
-        console.log('Erm, that just happened')
+        alert('Erm, that just happened')
     }
 }
 
-function getPlayerChoice() {
-    let playerHand = prompt('What hand would you like to play?', "");
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('it is a tie!')
+        console.log("#################")
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('Paper covers rock. You lose!')
+        console.log("#################")
+        ++computerScore
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('Rock crushes scissors! You win!')
+        console.log("#################")
+        ++playerScore
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You covered the enemy. You win!')
+        console.log("#################")
+        ++playerScore
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('No! You are being cut up! You lose!')
+        console.log("#################")
+        ++computerScore
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You are about to be crushed! You lose!')
+        console.log("#################")
+        ++computerScore
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You can cut them up! Great job! You win!')
+        console.log("#################")
+        ++playerScore
+    }
 }
 
-getComputerChoice(randomHand);
+let computerScore = 0;
+let playerScore = 0;
+
+function showScore(compScore, userScore) {
+    if (userScore === 0 && compScore === 0) {
+        return console.log('The player scored ' + playerScore + 
+            ' and the computer scored ' + computerScore + 
+            ' . Its a draw!')
+        
+    } else if (compScore > userScore) {
+        return console.log('The player scored ' + playerScore + 
+            ' and the computer scored ' + computerScore + 
+            ' . You lose!')
+        
+    } else if (userScore > compScore) {
+        return console.log('The player scored ' + playerScore + 
+            ' and the computer scored ' + computerScore + 
+            ' . You win!')
+    }
+}
+
+function playGame(count = 5) {
+    if (count <= 0) {
+        showScore(computerScore, playerScore);
+        return;
+    }
+   
+    const humanSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice(); 
+    
+    playRound(humanSelection, computerSelection);
+
+    playGame(count - 1);
+}
+
+playGame();
